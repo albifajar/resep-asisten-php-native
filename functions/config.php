4b06ee0b8e102933951beca2b939864a651d37e2
alfarama <albi@sawala.tech>
2022-07-02 20:19:55 +0700

    fix: url

--- functions/config.php
+++ functions/config.php
@@ -1,3 +1,3 @@
 <?php
 $connection = mysqli_connect('194.233.77.193:3306','uqfile0k_albi','@adminadmin!','uqfile0k_resep-asisten');
-define("BASE_URL", "http://localhost/resep-asisten-php-native");
+define("BASE_URL", "http://localhost/resep-asisten");
